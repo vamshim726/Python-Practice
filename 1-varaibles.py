55deb64fc811d7d55687147ed8a9ac972364d517
vamshim726 <vamshigithub@gmail.com>
2025-05-23 15:58:43 +0530

    varibales , swapping nums program

---
+++ 1-varaibles.py
@@ -0,0 +1,15 @@
+num1=10
+num2=20
+balance=10000
+name="vamshi"
+___a=20
+ordered=True
+print(f"{balance} - {name}  - {___a}")
+print("num 1 is {} num2 is {}".format(num1,num2))
+
+#swapping
+
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print(num1,num2)
